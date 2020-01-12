13f920f0b34b39a16c71969cd3d8de1e33d308d0
msimon42 <msimon@uab.edu>
2020-01-11 19:23:18 -0700

    Set up relationship and validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,10 @@
 class Coupon < ApplicationRecord
-  validates_uniquness_of :name, :code
+  validates_presence_of :name,
+                       :code,
+                       :percent_off
+
+  validates_uniquness_of :name,
+                         :code
+
   belongs_to :merchant
 end

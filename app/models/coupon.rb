ca279f650d1167d9927f50116eff60e904a864a0
msimon42 <msimon@uab.edu>
2020-01-12 22:43:57 -0700

    Change validation

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,10 +1,11 @@
 class Coupon < ApplicationRecord
   validates_presence_of :name,
-                       :code,
-                       :percent_off
+                       :code
 
   validates_uniqueness_of :name,
                          :code
+
+  validates_inclusion_of :percent_off, in: 0..100, message: 'Discount must be betweem 0 and 100%'                       
 
   belongs_to :merchant
   has_many :orders

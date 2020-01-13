f874d054072a5bde44677d58f3d91dcb2da3cb20
msimon42 <msimon@uab.edu>
2020-01-13 13:06:24 -0700

    Update validation test for coupon discount

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,7 +5,7 @@ class Coupon < ApplicationRecord
   validates_uniqueness_of :name,
                          :code
 
-  validates_inclusion_of :percent_off, in: 0..100, message: 'Discount must be betweem 0 and 100%'                       
+  validates_inclusion_of :percent_off, in: 0..100, message: 'Discount must be between 0 and 100%'                       
 
   belongs_to :merchant
   has_many :orders
--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe Coupon do
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :code}
-    it {should validate_presence_of :percent_off}
+    it {should validate_inclusion_of(:percent_off)
+              .in_range(0..100)
+              .with_message('Discount must be between 0 and 100%')}
     it {should validate_uniqueness_of :name}
     it {should validate_uniqueness_of :code}
   end

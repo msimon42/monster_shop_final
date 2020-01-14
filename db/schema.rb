120b01ad35aabd9b73fb7932eadf16a566b6a383
msimon42 <msimon@uab.edu>
2020-01-13 18:58:53 -0700

    Remove discount from orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200113230521) do
+ActiveRecord::Schema.define(version: 20200114005627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 20200113230521) do
     t.bigint "user_id"
     t.integer "status", default: 0
     t.bigint "coupon_id"
-    t.float "discount", default: 0.0
     t.index ["coupon_id"], name: "index_orders_on_coupon_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end

4cd7abf39a55d3ea45141ae02d7d42798d433777
msimon42 <msimon@uab.edu>
2020-01-14 11:35:51 -0700

    Move order-item creation process to helper methods

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -11,17 +11,11 @@ class User::OrdersController < ApplicationController
 
   def create
     order = current_user.orders.create
-    order.coupon_id = cart.applied_coupon['id'] if cart.applied_coupon['id']
-      cart.items.each do |item|
-        order.order_items.create({
-          item: item,
-          quantity: cart.count_of(item.id),
-          price: item.price,
-          discount: cart.discount_amount(item),
-          revenue: (item.price * cart.count_of(item.id)) - cart.discount_amount(item)
-          })
-      end
-      #binding.pry
+    if cart.applied_coupon['id']
+      create_order_with_coupon
+    else
+      create_order_without_coupon
+    end
     session.delete(:cart)
     session.delete(:coupon)
     flash[:notice] = "Order created successfully!"
@@ -32,5 +26,33 @@ class User::OrdersController < ApplicationController
     order = current_user.orders.find(params[:id])
     order.cancel
     redirect_to "/profile/orders/#{order.id}"
+  end
+
+  private
+
+  def create_order_with_coupon
+    order = current_user.orders.create(coupon_id: cart.applied_coupon['id'])
+    cart.items.each do |item|
+      order.order_items.create({
+        item: item,
+        quantity: cart.count_of(item.id),
+        price: item.price,
+        discount: cart.discount_amount(item),
+        revenue: (item.price * cart.count_of(item.id)) - cart.discount_amount(item)
+        })
+    end
+  end
+
+  def create_order_without_coupon
+    order = current_user.orders.create
+    cart.items.each do |item|
+      order.order_items.create({
+        item: item,
+        quantity: cart.count_of(item.id),
+        price: item.price,
+        discount: 0,
+        revenue: item.price * cart.count_of(item.id)
+        })
+    end
   end
 end
